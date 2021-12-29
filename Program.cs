4e74bdb678d8e5ff06a696bf1197e76a7a44cb17
msla-InspireSA <80754340+msla-InspireSA@users.noreply.github.com>
2021-12-29 23:26:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Azure;
+using System.Globalization;
+using Azure.AI.TextAnalytics;
+
+namespace LinkedEntities
+{
+    internal class Program
+    {
+        //connecttion to azure
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("use your own key");
+        private static readonly Uri endpoint = new Uri("use your own endpoint");
+
+        // Example method for recognizing entities and providing a link to an online data source
+        static void EntityLinkingExample(TextAnalyticsClient client)
+        {
+            var response = client.RecognizeLinkedEntities(
+                "Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975, " +
+                "to develop and sell BASIC interpreters for the Altair 8800. " +
+                "During his career at Microsoft, Gates held the positions of chairman, " +
+                "chief executive officer, president and chief software architect, " +
+                "while also being the largest individual shareholder until May 2014.");
+            Console.WriteLine("Linked Entities:");
+            foreach (var entity in response.Value)
+            {
+                Console.WriteLine($"\tName: {entity.Name},\tID: {entity.DataSourceEntityId},\tURL: {entity.Url}\tData Source: {entity.DataSource}");
+                Console.WriteLine("\tMatches:");
+                foreach (var match in entity.Matches)
+                {
+                    Console.WriteLine($"\t\tText: {match.Text}");
+                    Console.WriteLine($"\t\tScore: {match.ConfidenceScore:F2}\n");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            EntityLinkingExample(client);
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
